4ef2b5787956db111fdd33eb39ef2f1a53539d18
Dan Allen <justdanallen@gmail.com>
2021-01-30 22:58:45 -0700

    add icons to webpage

--- website/app/index.tsx
+++ website/app/index.tsx
@@ -6,9 +6,12 @@ import {
   ChakraProps,
   useColorMode,
   IconButton,
+  VisuallyHidden,
 } from '@chakra-ui/react';
 import { IconSearch } from './icon-search';
 import { MoonIcon, SunnyIcon } from '../../src';
+import { ChakraLogo } from './chakra-logo';
+import { IonIconLogo } from './ionicons-logo';
 
 export function App() {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -20,10 +23,21 @@ export function App() {
 
   return (
     <Box as="main" padding={[3, 5]} maxWidth="900px" m="0 auto">
-      <Heading {...fullWidthCenter}>Chakra + Ion Icons</Heading>
-      <Text {...fullWidthCenter} color="gray.500" my={[5, 7]}>
+      <Heading
+        {...fullWidthCenter}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <VisuallyHidden>Chakra</VisuallyHidden>
+        <ChakraLogo height="100%" boxSize={[32, 48, 60]} mr={[3, 5]} /> +
+        <VisuallyHidden>Ion Icons</VisuallyHidden>
+        <IonIconLogo height="100%" boxSize={[32, 48, 60]} ml={[3, 5]} />
+      </Heading>
+      <Text {...fullWidthCenter} color="gray.500" mb={[5, 7]}>
         Chakra compatible Ion Icons. Click on an icon to see usage.
       </Text>
+
       <IconSearch
         onSelect={icon => console.log(icon)}
         options={
