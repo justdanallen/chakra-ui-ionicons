ddaf943cd0526a9a7eab536ad78e21025ece6ac0
Dan Allen <justdanallen@gmail.com>
2021-01-30 23:17:13 -0700

    add github and npm links

--- website/app/index.tsx
+++ website/app/index.tsx
@@ -7,9 +7,10 @@ import {
   useColorMode,
   IconButton,
   VisuallyHidden,
+  Flex,
 } from '@chakra-ui/react';
 import { IconSearch } from './icon-search';
-import { MoonIcon, SunnyIcon } from '../../src';
+import { LogoGithubIcon, LogoNpmIcon, MoonIcon, SunnyIcon } from '../../src';
 import { ChakraLogo } from './chakra-logo';
 import { IonIconLogo } from './ionicons-logo';
 
@@ -22,32 +23,59 @@ export function App() {
   };
 
   return (
-    <Box as="main" padding={[3, 5]} maxWidth="900px" m="0 auto">
-      <Heading
-        {...fullWidthCenter}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
+    <>
+      <Box as="main" padding={[3, 5]} maxWidth="900px" m="0 auto">
+        <Heading
+          {...fullWidthCenter}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <VisuallyHidden>Chakra</VisuallyHidden>
+          <ChakraLogo height="100%" boxSize={[32, 48, 60]} mr={[3, 5]} /> +
+          <VisuallyHidden>Ion Icons</VisuallyHidden>
+          <IonIconLogo height="100%" boxSize={[32, 48, 60]} ml={[3, 5]} />
+        </Heading>
+        <Text {...fullWidthCenter} color="gray.500" mb={[5, 7]}>
+          Chakra compatible Ion Icons. Click on an icon to see usage.
+        </Text>
+        <IconSearch
+          onSelect={icon => console.log(icon)}
+          options={
+            <IconButton
+              onClick={toggleColorMode}
+              aria-label="change color mode"
+              icon={colorMode === 'light' ? <MoonIcon /> : <SunnyIcon />}
+            />
+          }
+        />
+      </Box>
+      <Flex
+        position="absolute"
+        top={0}
+        right={0}
+        p={4}
+        sx={{
+          '> a': {
+            ml: 2,
+          },
+        }}
       >
-        <VisuallyHidden>Chakra</VisuallyHidden>
-        <ChakraLogo height="100%" boxSize={[32, 48, 60]} mr={[3, 5]} /> +
-        <VisuallyHidden>Ion Icons</VisuallyHidden>
-        <IonIconLogo height="100%" boxSize={[32, 48, 60]} ml={[3, 5]} />
-      </Heading>
-      <Text {...fullWidthCenter} color="gray.500" mb={[5, 7]}>
-        Chakra compatible Ion Icons. Click on an icon to see usage.
-      </Text>
-
-      <IconSearch
-        onSelect={icon => console.log(icon)}
-        options={
-          <IconButton
-            onClick={toggleColorMode}
-            aria-label="change color mode"
-            icon={colorMode === 'light' ? <MoonIcon /> : <SunnyIcon />}
-          />
-        }
-      />
-    </Box>
+        <IconButton
+          aria-label="github repo"
+          as="a"
+          href={'https://github.com/justdanallen/chakra-ui-ionicons'}
+          rel="noopener norefferer nofollow"
+          icon={<LogoGithubIcon />}
+        />
+        <IconButton
+          aria-label="go to npm package"
+          as="a"
+          href={'https://www.npmjs.com/package/chakra-ui-ionicons'}
+          rel="noopener norefferer nofollow"
+          icon={<LogoNpmIcon />}
+        />
+      </Flex>
+    </>
   );
 }
